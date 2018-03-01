9e5cc63e2f649df2e127df2d38be47c8dfd4ff8b
nlevo <nikola.levo@gmail.com>
2018-03-01 18:18:21 -0500

    Set port 2

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ const bcrypt         = require('bcrypt');
 
 
 app.set('port', (process.env.PORT || 5000));
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+  var result = 'App is running'
+  response.send(result);
+}).listen(app.get('port'), function() {
+  console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 require("dotenv").config();
 mongoose.connect(process.env.MONGODB_URI);
