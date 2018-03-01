b6378519dced31b482b66c6bcee63755b8714f78
nlevo <nikola.levo@gmail.com>
2018-03-01 17:50:11 -0500

    Trying to fix heroku app

---
+++ app.js
@@ -0,0 +1,150 @@
+
+const express        = require("express");
+const path           = require("path");
+const logger         = require("morgan");
+const cookieParser   = require("cookie-parser");
+const bodyParser     = require("body-parser");
+const mongoose       = require("mongoose");
+const zxcvbn         = require('zxcvbn')
+const app            = express();
+
+const expressLayouts = require('express-ejs-layouts');
+const passport       = require('passport');
+const LocalStrategy  = require('passport-local').Strategy;
+const session        = require("express-session");
+const User           = require('./models/user');
+const bcrypt         = require('bcrypt');
+// Controllers
+
+require("dotenv").config();
+mongoose.connect(process.env.MONGODB_URI);
+// Mongoose configuration
+//mongoose.connect("mongodb://localhost/basic-auth");
+
+// Middlewares configuration
+app.use(logger("dev"));
+const authRoutes = require('./routes/auth-routes');
+const siteRoutes = require('./routes/site-routes');
+
+//Layouts config
+app.use(expressLayouts);
+app.set('layout', 'layouts/main-layout');
+
+// View engine configuration
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+
+// Access POST params with body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({
+  secret: "basic-auth-secret",
+  resave: true,
+  saveUninitialized: true
+}));
+
+//passport configß
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+// Signing Up
+passport.use('local-signup', new LocalStrategy(
+  { passReqToCallback: true },
+  (req, username, password, next) => {
+    // To avoid race conditions
+    process.nextTick(() => {
+        User.findOne({
+            'username': username
+        }, (err, user) => {
+            if (err){ return next(err); }
+
+            if (user) {
+                return next(null, false);
+            } else {
+                // Destructure the body
+                const { username, email, description, password } = req.body;
+                const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+                const newUser = new User({
+                  username,
+                  email,
+                  description,
+                  password: hashPass
+                });
+
+                newUser.save((err) => {
+                    if (err){ next(err); }
+                    return next(null, newUser);
+                });
+            }
+        });
+    });
+}));
+
+//login config
+passport.use('local-login', new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Authentication
+app.use(cookieParser());
+
+app.use( (req, res, next) => {
+  if (typeof(req.user) !== "undefined"){
+    res.locals.userSignedIn = true;
+  } else {
+    res.locals.userSignedIn = false;
+  }
+  next();
+});
+
+// Routes
+app.use('/', authRoutes);
+app.use('/', siteRoutes);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ routes/site-routes.js
@@ -0,0 +1,166 @@
+const express        = require("express");
+const siteRoutes     = express.Router();
+const passport       = require('passport');
+const ensureLogin    = require('connect-ensure-login');
+const Business       = require('../models/business');
+var geocoder         = require('geocoder');
+
+
+ 
+
+
+  //ROUTE DISPLAY NEW BUSINESS FORM
+  siteRoutes.get("/new", ensureLogin.ensureLoggedIn(), (req, res, next) => {
+    
+    res.render("private/new", {categories: (Business.schema.path('category').enumValues), //categories auto-population
+                               languages: (Business.schema.path('language').enumValues)}); //language auto-population
+  });
+
+  //ROUTE POST ADD NEW BUSINESS
+  siteRoutes.post('/new', ensureLogin.ensureLoggedIn('/login'), (req, res, next) => {
+
+    //geocoding address to longitude and langitude
+    geocoder.geocode(req.body.full_address, function(err, data) {
+      if(err || data.results === "undefined"){console.log("Error: " + err); res.render('error');}
+    let x = Object.values(data.results[0].geometry.location);
+    let location = {
+      type: 'Point',
+      coordinates: [x[0], x[1]]
+    };
+      //create new business object
+      const newBusiness = new Business({
+        category: req.body.category,
+        name: req.body.name,
+        phone: req.body.phone,
+        description: req.body.description,
+        language: req.body.language,
+        website: req.body.website,
+        loc: location,
+        // We're assuming a user is logged in here
+        // If they aren't, this will throw an error
+        _owner: req.user._id,
+        hours: {
+          mon: {open: req.body.open, close: req.body.close, isClosed: req.body.isClosed}
+        },
+        address: {
+          street_num: Number(req.body.street_num),
+          street_name: req.body.street_name,
+          city: req.body.city,
+          state: req.body.state,
+          zip: Number(req.body.zip)
+        },
+      });
+
+      newBusiness.save( (err) => {
+        if (err) {
+          res.send(`<h1>${err}</h1>`);   
+        } else {
+          res.redirect(`/list`);
+        }
+      });
+    });
+  });
+
+  //ROUTE GENERATE LIST OF BUSINESSES
+  siteRoutes.get('/list', (req, res, next) => {
+  
+    
+    Business.find({},function(err,data){
+      if (err){
+          res.render("error", {message: err})
+      } 
+      let userID;
+      if(req.user) {userID = req.user._id;}
+      else              {userID = 0;}
+      res.render("list", { businesses: data, userId: userID  });
+    });  
+  });
+
+   //GET UPDATE FORM
+   siteRoutes.get('/business/:id/edit', ensureLogin.ensureLoggedIn('/login'), (req, res, next) => {
+    // New
+    let businessId = req.params.id;
+
+    Business.findById( businessId, function(err,data){
+      if (err){
+          res.render("error", {message: err})
+      } 
+      res.render("private/edit2", { business: data, 
+                                   categories: (Business.schema.path('category').enumValues), //categories auto-population
+                                   languages: (Business.schema.path('language').enumValues)}  //languages auto-population
+    );  
+  });
+});
+
+  //ROUTE - DELETE BUSINESS
+  siteRoutes.get('/business/:id/delete', ensureLogin.ensureLoggedIn('/login'), (req, res, next) => {
+    const id = req.params.id;
+    console.log(id);
+    Business.findOneAndRemove(id, function(err,data){
+      if (err){
+          res.render("error", {message: err})
+      } 
+      res.redirect("/list");
+    });  
+  });
+  
+  //POST UPDATE FORM
+  siteRoutes.post('/business/:id/edit', ensureLogin.ensureLoggedIn('/login'),(req, res, next) => {
+    // New
+    const updatedBusiness = {
+      category: req.body.category,
+      name: req.body.name,
+      phone: req.body.phone,
+      description: req.body.description,
+      language: req.body.language,
+      website: req.body.website,
+    };
+
+    Business.findByIdAndUpdate(req.params.id, updatedBusiness, function(err,data){
+      if (err){
+          res.render("error", {message: err})
+      } 
+      res.redirect("/list");
+    });  
+  });
+
+//POST - SEARCH BUSINESS
+  siteRoutes.post('/search',(req, res, next) => {
+
+    Business.find( {category: req.body.category, language: req.body.language}, function(err,data){
+      if (err){
+          res.render("error", {message: err})
+      } 
+      res.render("map-search", { business: data, 
+        categories: (Business.schema.path('category').enumValues), //categories auto-population
+        languages: (Business.schema.path('language').enumValues)});
+    });  
+  });
+
+  // //new databse route
+  // siteRoutes.get('/getBusiness', (req, res, next) => {
+  //   console.log()
+  //   Business.find((err, buissnesses) => {
+  //     res.json(buissnesses);
+  //   })
+  // })
+
+  //ROUTE TO MAP
+siteRoutes.get("/", (req, res, next) => {
+  res.render("map", { categories: Business.schema.path('category').enumValues, //categories auto-population
+                      languages: Business.schema.path('language').enumValues });
+});
+
+  
+
+
+function stringTimeToMinutes(stringTime){
+  let hrs, min, totalInMinutes;
+  hrs = Number(stringTime.slice(0, 2));
+  min = Number(stringTime.slice(3, 5));
+  totalInMinutes = (hrs*60) + min;
+  return totalInMinutes;
+}
+module.exports = siteRoutes;
+
+
