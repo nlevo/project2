f8066b17eb15f6d8d26fc0da1fd199c3dc7e26f3
nlevo <nikola.levo@gmail.com>
2018-03-01 19:13:27 -0500

    HEROKUgit add .

--- app.js
+++ app.js
@@ -17,13 +17,8 @@ const bcrypt         = require('bcrypt');
 // Controllers
 
 
-app.set('port', (process.env.PORT || 5000));
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-  var result = 'App is running'
-  response.send(result);
-}).listen(app.get('port'), function() {
-  console.log('App is running, server is listening on port ', app.get('port'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 require("dotenv").config();
